e7a25bb6b4aaaaec58c837927556b51308b3223f
Ash Porter <ashley.porter@eyecuelab.com>
2023-04-25 12:08:32 -0700

    update configs, remove extra

--- src/__tests__/line-number.test.ts
+++ src/__tests__/line-number.test.ts
@@ -1,23 +1,9 @@
-import { generateAreaCode } from "../generate/area-code";
+import { describe, expect, it } from "vitest";
+import { generateLineNumber } from "../generate/line-number";
 
-const areaCodeRegex = /^[2-9](\d)(?!\1)\d$/;
-
-describe(`Area Code`, () => {
-	it(`should generate a valid area code`, () => {
-		const areaCode = generateAreaCode();
-		expect(areaCode).toMatch(areaCodeRegex);
-	});
-	it(`should allow a valid area code to be passed in`, () => {
-		const areaCode = generateAreaCode(`503`);
-		expect(areaCode).toBe(`503`);
-	});
-	it(`should not allow an invalid area code to be passed in`, () => {
-		try {
-			generateAreaCode(`911`);
-		} catch (error) {
-			if (error instanceof Error) {
-				expect(error.message).toBe(`Invalid area code`);
-			}
-		}
+describe(`Line number`, () => {
+	it(`should generate a valid line number`, () => {
+		const lineNumber = generateLineNumber();
+		expect(lineNumber.length).toBe(4);
 	});
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,11 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: `jsdom`,
-		include: [`**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}`],
+		include: [`**/*.{test,spec}.ts`],
 		exclude: [
 			`**/node_modules/**`,
 			`**/dist/**`,
+			`**/lib/**`,
 			`**/cypress/**`,
 			`**/.{idea,git,cache,output,temp}/**`,
 			`**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*`,
