a1bde122611cb802b910884126b3ac16aad9e9ce
Ash Porter <ashley.porter@eyecuelab.com>
2023-04-26 17:57:27 -0700

    add extra fictionalize option and overrides to the overall number generator

--- src/generate/nanp-number.ts
+++ src/generate/nanp-number.ts
@@ -1,17 +1,46 @@
-import { generateAreaCode } from "./area-code.js";
-import { generateCentralOfficeCode } from "./central-office-code.js";
-import { generateLineNumber } from "./line-number.js";
+// LOGIC
 
-export const generateNANPNumber = () => {
-	const areaCode = generateAreaCode();
-	const officeCode = generateCentralOfficeCode();
-	if (officeCode === `555`) {
-		// if this is a 555 number, the next 4 can only be 01XX
-		const lineNumber = generateLineNumber(`555`, true);
-		// not formatting the number, that should be front-end stuff
-		return `${areaCode}${officeCode}${lineNumber}`;
+import { generateAreaCode } from "./area-code";
+import { generateCentralOfficeCode } from "./central-office-code";
+import { generateLineNumber } from "./line-number";
+
+/**
+ * Generates a NANP number
+ * 
+ * Add in ability to override the number's components
+ * Add in ability to fictionalize the number with 555-01XX pattern
+ */
+
+export const generateNANPNumber = (options: {
+  areaCode?: string,
+  centralOfficeCode?: string,
+  lineNumber?: string,
+  fictionalize?: boolean,
+}) => {
+	let {
+		areaCode,
+		centralOfficeCode,
+		lineNumber,
+	} = options;
+	const {
+		fictionalize,
+	} = options;
+	// Area code will be assigned if not provided
+	if (!areaCode) {
+		areaCode = generateAreaCode();
 	}
-	const lineNumber = generateLineNumber();
-	// not formatting the number, that should be front-end stuff
-	return `${areaCode}${officeCode}${lineNumber}`;
+	// Central office code will be assigned if not provided
+	if (!centralOfficeCode) {
+		centralOfficeCode = generateCentralOfficeCode();
+	}
+	// Line number will be assigned if not provided
+	if (!lineNumber) {
+		lineNumber = generateLineNumber();
+	}
+	// If fictionalize is true, the number will be 555-01XX
+	if (fictionalize) {
+		centralOfficeCode = `555`;
+		lineNumber = generateLineNumber(undefined, true);
+	}
+	return `${areaCode}${centralOfficeCode}${lineNumber}`;
 };
