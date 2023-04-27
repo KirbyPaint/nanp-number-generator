263e757d8f3e996f981d2fc7317dbe63ce3a4b3e
Ash Porter <ashley.porter@eyecuelab.com>
2023-04-26 18:31:20 -0700

    add additional inputs to generate fictional numbers, tests

--- src/generate/nanp-number.ts
+++ src/generate/nanp-number.ts
@@ -1,8 +1,18 @@
-// LOGIC
+import { validateAreaCode } from "../validate/area-code";
+import { validateCentralOfficeCode } from "../validate/central-office-code";
+import { validateLineNumber } from "../validate/line-number";
 
+// imports are sorted alphabetically!!!
+// eslint-disable-next-line sort-imports
 import { generateAreaCode } from "./area-code";
 import { generateCentralOfficeCode } from "./central-office-code";
 import { generateLineNumber } from "./line-number";
+
+type Options = {
+  areaCode?: string;
+  centralOfficeCode?: string;
+  lineNumber?: string;
+};
 
 /**
  * Generates a NANP number
@@ -10,37 +20,26 @@ import { generateLineNumber } from "./line-number";
  * Add in ability to override the number's components
  * Add in ability to fictionalize the number with 555-01XX pattern
  */
-
-export const generateNANPNumber = (options: {
-  areaCode?: string,
-  centralOfficeCode?: string,
-  lineNumber?: string,
-  fictionalize?: boolean,
-}) => {
-	let {
-		areaCode,
-		centralOfficeCode,
-		lineNumber,
-	} = options;
-	const {
-		fictionalize,
-	} = options;
-	// Area code will be assigned if not provided
-	if (!areaCode) {
-		areaCode = generateAreaCode();
+export const generateNANPNumber = (options: boolean | Options) => {
+	if (typeof options === `boolean`) {
+		const areaCode = generateAreaCode();
+		const centralOfficeCode = `555`;
+		const lineNumber = generateLineNumber(undefined, true);
+		return `${areaCode}${centralOfficeCode}${lineNumber}`;
+	} else {
+		let { areaCode, centralOfficeCode, lineNumber } = options;
+		// Area code will be assigned if not provided
+		if (!areaCode || !validateAreaCode(areaCode)) {
+			areaCode = generateAreaCode();
+		}
+		// Central office code will be assigned if not provided
+		if (!centralOfficeCode || !validateCentralOfficeCode(centralOfficeCode)) {
+			centralOfficeCode = generateCentralOfficeCode();
+		}
+		// Line number will be assigned if not provided
+		if (!lineNumber || !validateLineNumber(lineNumber)) {
+			lineNumber = generateLineNumber();
+		}
+		return `${areaCode}${centralOfficeCode}${lineNumber}`;
 	}
-	// Central office code will be assigned if not provided
-	if (!centralOfficeCode) {
-		centralOfficeCode = generateCentralOfficeCode();
-	}
-	// Line number will be assigned if not provided
-	if (!lineNumber) {
-		lineNumber = generateLineNumber();
-	}
-	// If fictionalize is true, the number will be 555-01XX
-	if (fictionalize) {
-		centralOfficeCode = `555`;
-		lineNumber = generateLineNumber(undefined, true);
-	}
-	return `${areaCode}${centralOfficeCode}${lineNumber}`;
 };
